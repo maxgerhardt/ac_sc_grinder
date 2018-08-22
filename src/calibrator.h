28a19a99b2eec1c187cdfc31c39461e60d925d87
Vitaly Puzrin <vitaly@rcdesign.ru>
2018-08-23 00:38:07 +0300

    Split calibrator switch to smaller fns

--- src/calibrator.h
+++ src/calibrator.h
@@ -22,80 +22,14 @@ class Calibrator
 public:
 
   bool tick(void) {
-    fix16_t knob = sensors.knob;
-
     switch (cal_state) {
-
-      case IDLE:
-
-        if (knob < KNOB_TRESHOLD)
-        {
-          cal_state_cnt++;
-          if (cal_state_cnt > knob_wait_min) set_state(UP_CHECK);
-        }
-        else reset();
-
-        break;
-
-      case UP_CHECK:
-        if (knob < KNOB_TRESHOLD && cal_state_cnt == 0) break;
-
-        if (knob >= KNOB_TRESHOLD)
-        {
-          cal_state_cnt++;
-          if (cal_state_cnt > knob_wait_max) reset();
-          break;
-        }
-
-        if (cal_state_cnt > knob_wait_min)
-        {
-          cal_knob_dials++;
-          if (cal_knob_dials >= 3) set_state(PRE_PAUSE);
-          else set_state(DOWN_CHECK);
-        }
-        else reset();
-
-        break;
-
-      case DOWN_CHECK:
-        if (knob < KNOB_TRESHOLD) {
-          cal_state_cnt++;
-          if (cal_state_cnt > knob_wait_max) reset();
-          break;
-        }
-
-        if (cal_state_cnt > knob_wait_min) set_state(UP_CHECK);
-        else reset();
-
-        break;
-
-      // From now we are in calibration more. Should care about triac and
-      // return true
-
-      case PRE_PAUSE:
-        // 1 sec pause to stop motor for sure
-        triacDriver.voltage = sensors.voltage;
-        triacDriver.setpoint = 0;
-        triacDriver.tick();
-
-        if (cal_state_cnt++ > 1 * APP_TICK_FREQUENCY) set_state(CALIBRATE_STATIC);
-
-        return true;
-
-      case CALIBRATE_STATIC:
-        // Dummy stub, 2 sec max speed to show it works
-        triacDriver.voltage = sensors.voltage;
-        triacDriver.setpoint = fix16_one;
-        triacDriver.tick();
-
-        if (cal_state_cnt++ > 2 * APP_TICK_FREQUENCY) reset();
-
-        return true;
-
-      default:
-        reset();
+      case IDLE:        return idle();
+      case UP_CHECK:    return up_check();
+      case DOWN_CHECK:  return down_check();
+      case PRE_PAUSE:   return pre_pause();
+      case CALIBRATE_STATIC: return calibrate_static();
+      default: reset(); return false;
     }
-    return false;
   }
 
 private:
@@ -105,10 +39,10 @@ private:
     UP_CHECK,
     DOWN_CHECK,
     PRE_PAUSE,
-    CALIBRATE_STATIC,
-    CALIBRATE_DYNAMIC
+    CALIBRATE_STATIC
   } cal_state = IDLE;
 
+  // Helper variables to reduce sub-states
   int cal_state_cnt = 0;
   int cal_knob_dials = 0;
 
@@ -122,6 +56,90 @@ private:
   {
     cal_knob_dials = 0;
     set_state(IDLE);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // States logic
+
+  bool idle(void)
+  {
+    fix16_t knob = sensors.knob;
+
+    if (knob < KNOB_TRESHOLD)
+    {
+      cal_state_cnt++;
+      if (cal_state_cnt > knob_wait_min) set_state(UP_CHECK);
+    }
+    else reset();
+
+    return false;
+  }
+
+  bool up_check(void)
+  {
+    fix16_t knob = sensors.knob;
+
+    if (knob < KNOB_TRESHOLD && cal_state_cnt == 0) return false;
+
+    if (knob >= KNOB_TRESHOLD)
+    {
+      cal_state_cnt++;
+      if (cal_state_cnt > knob_wait_max) reset();
+      return false;
+    }
+
+    if (cal_state_cnt > knob_wait_min)
+    {
+      cal_knob_dials++;
+      if (cal_knob_dials >= 3) set_state(PRE_PAUSE);
+      else set_state(DOWN_CHECK);
+    }
+    else reset();
+
+    return false;
+  }
+
+  bool down_check(void)
+  {
+    fix16_t knob = sensors.knob;
+
+    if (knob < KNOB_TRESHOLD) {
+      cal_state_cnt++;
+      if (cal_state_cnt > knob_wait_max) reset();
+      return false;
+    }
+
+    if (cal_state_cnt > knob_wait_min) set_state(UP_CHECK);
+    else reset();
+
+    return false;
+  }
+
+  // From now we are in calibration mode. Should care about triac and
+  // return true
+
+  bool pre_pause(void)
+  {
+    // 1 sec pause to stop motor for sure
+    triacDriver.voltage = sensors.voltage;
+    triacDriver.setpoint = 0;
+    triacDriver.tick();
+
+    if (cal_state_cnt++ > 1 * APP_TICK_FREQUENCY) set_state(CALIBRATE_STATIC);
+
+    return true;
+  }
+
+  bool calibrate_static(void)
+  {
+    // Dummy stub, 2 sec max speed to show it works
+    triacDriver.voltage = sensors.voltage;
+    triacDriver.setpoint = fix16_one;
+    triacDriver.tick();
+
+    if (cal_state_cnt++ > 2 * APP_TICK_FREQUENCY) reset();
+
+    return true;
   }
 };
 
